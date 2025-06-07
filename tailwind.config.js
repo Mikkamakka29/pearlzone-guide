d506be316b65c4b17da96ae0aac60953a9921660
Adam D <deriadam@hotmail.com>
2025-06-07 17:53:30 +0200

    design: center container utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
   
     theme: {
       extend: {
+        container: {
+          center: true,
+          padding: '1rem',
+          screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+          },
+        },
         /* ─── Design-system palette ─────────────────────────────── */
         colors: {
           primary: "#0B6AFF", // Danube blue
